421f394f328690b66a116ead3a3ca6e0244809d4
Vaibhav Malhotra <vaimalho@adobe.com>
2020-09-12 23:33:03 +0530

    Adding O(1) solution

--- LeetCode_August/PowerOfFour.java
+++ LeetCode_August/PowerOfFour.java
@@ -34,4 +34,9 @@ public class PowerOfFour {
         }
 		return false;
     }
+	
+	// Time Complexity: O(1)
+	public static boolean isPowerOfFour1(int num) {
+			return num>0 && ((num & (num-1))==0) && num%3==1;
+	}
 }
