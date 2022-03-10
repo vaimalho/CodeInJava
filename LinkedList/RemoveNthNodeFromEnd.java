2fca2e581a50bea39864f3d1bddbc3ed4d641f06
vaimalho <66966469+vaimalho@users.noreply.github.com>
2022-03-10 23:21:27 +0530

    Update RemoveNthNodeFromEnd.java

--- LinkedList/RemoveNthNodeFromEnd.java
+++ LinkedList/RemoveNthNodeFromEnd.java
@@ -17,79 +17,90 @@ Example 3:
 
 Input: head = [1,2], n = 1
 Output: [1]
-*/		
+ */
 
 package linkedlist;
 
 public class RemoveNthNodeFromEnd {
 
-	static class ListNode {
-		int val;
-		ListNode next;
-		ListNode(int x) {
-			val = x;
-			next = null;
-		}
-	}
-	
-	
-	public static ListNode removeNthFromEnd(ListNode head, int n) {
-		
-		ListNode curr = head;
-		int size=0;
-		while(curr!=null) {
-			curr=curr.next;
-			size++;
-		}
-		
-		int index = size-n;
-		if(index<0) {
-			return head;
-		}
-		if(index==0) {
-			head=head.next;
-			return head;
-		}
-			
-		curr=head;
-		for(int i=1; i<index; i++) {
-			curr=curr.next;
-		}
-		
-		curr.next=curr.next.next;
-		return head;
+    static class ListNode {
+	int val;
+	ListNode next;
 
+	ListNode(int x) {
+	    val = x;
+	    next = null;
 	}
-	
-	public static ListNode removeNthFromEnd2(ListNode head, int n) {
-		
-		ListNode dummy = new ListNode(0);
-		dummy.next=head;
-		ListNode first = dummy;
-		ListNode second = dummy;
-		
-		for(int i=0 ; i<=n; i++) {
-			first=first.next;
-		}
-		while(first!=null) {
-			first=first.next;
-			second=second.next;
-		}
-		second.next = second.next.next;
-		return dummy.next;
+    }
 
+    // Two Pass Algorithm
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+
+	ListNode curr = head;
+	int len = 0;
+	int diff = 0;
+
+	while (curr != null) {
+	    len++;
+	    curr = curr.next;
+	}
+	curr = head;
+
+	diff = len - n;
+
+	if (diff == 0) {
+	    return head.next;
+	}
+	if (diff > 0) {
+	    for (int i = 0; i < diff - 1; i++) {
+		curr = curr.next;
+	    }
+	    curr.next = curr.next.next;
 	}
 
+	return head;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ListNode head = new ListNode(1);
-		head.next = new ListNode(2);
-		head.next.next = new ListNode(3);
-		head.next.next.next = new ListNode(4);
-		head.next.next.next.next = new ListNode(5);
-		
-		removeNthFromEnd2(head,2);
+    }
+
+    // One Pass Algorithm
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public static ListNode removeNthFromEnd2(ListNode head, int n) {
+
+	// move current node to n
+	ListNode currNode = head;
+
+	for (int i = 0; i < n; i++) {
+	    currNode = currNode.next;
 	}
+
+	if (currNode == null)
+	    return head.next;
+
+	ListNode nodeBeforeN = head;
+
+	while (currNode.next != null) {
+	    nodeBeforeN = nodeBeforeN.next;
+	    currNode = currNode.next;
+	}
+
+	nodeBeforeN.next = nodeBeforeN.next.next;
+
+	return head;
+
+    }
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	ListNode head = new ListNode(1);
+	head.next = new ListNode(2);
+	head.next.next = new ListNode(3);
+	head.next.next.next = new ListNode(4);
+	head.next.next.next.next = new ListNode(5);
+
+	removeNthFromEnd2(head, 2);
+    }
 
 }
