e7b69ce46f0f77e50e19013e4c1bc48c9e97370b
vaimalho <66966469+vaimalho@users.noreply.github.com>
2022-03-15 14:17:03 +0530

    Update ReverseSingleLinkedListMain.java

--- LinkedList/ReverseSingleLinkedListMain.java
+++ LinkedList/ReverseSingleLinkedListMain.java
@@ -1,3 +1,34 @@
+/* Reverse Linked List
+
+Solution
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+ 
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+*/
+
 package linkedlist;
 
 public class ReverseSingleLinkedListMain {
@@ -26,6 +57,8 @@ public class ReverseSingleLinkedListMain {
 		System.out.println();
 	}
 	
+	// Time Complexity: O(N)
+	// Space Complexity: O(1)
 	public static Node reverseList(Node head) {
 		Node prev=null;
 		Node curr=head;
@@ -38,6 +71,21 @@ public class ReverseSingleLinkedListMain {
 			curr=next;
 		}
 		return prev;
+	}
+
+	// Time Complexity: O(N)
+	// Space Complexity: O(N)
+	public static Node reverseList1(Node head) {
+
+	    if (head == null || head.next == null)
+		return head;
+
+	    Node newHead = reverseList1(head.next);
+	    head.next.next = head;
+	    head.next = null;
+
+	    return newHead;
+
 	}
 		
 	public static void main(String[] args) {
