3955ee6b7e8a64fdaafc46a403fef92b41e85ac8
vaimalho <66966469+vaimalho@users.noreply.github.com>
2022-08-03 13:25:55 +0530

    Update ThreadDemo10.java

--- multithreading/ThreadDemo10.java
+++ multithreading/ThreadDemo10.java
@@ -4,9 +4,19 @@ public class ThreadDemo10 {
 
     public static void main(String[] args) {
 
+        System.out.println(Thread.currentThread().getPriority());
 
+        Thread t = new Thread();
+        System.out.println(t.getPriority());
 
+        // RTE: Valid Range: 1 - 10
+        //Thread.currentThread().setPriority(15);
 
+        Thread.currentThread().setPriority(9);
+
+        Thread t1 = new Thread();
+        System.out.println(Thread.currentThread().getPriority());
+        System.out.println(t1.getPriority());
     }
 
 }
