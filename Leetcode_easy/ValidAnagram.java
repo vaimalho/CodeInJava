cb8e9dfc49ff5494205d0c987563c5c43a0db709
vaimalho <66966469+vaimalho@users.noreply.github.com>
2024-05-05 21:12:04 +0530

    Update ValidAnagram.java

--- Leetcode_easy/ValidAnagram.java
+++ Leetcode_easy/ValidAnagram.java
@@ -1,105 +1,74 @@
-/*
-Valid Anagram
+package interviewprep;
 
-Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-
-An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
-typically using all the original letters exactly once.
-
-Example 1:
-
-Input: s = "anagram", t = "nagaram"
-Output: true
-Example 2:
-
-Input: s = "rat", t = "car"
-Output: false
-*/
-
-package leetcodeeasy;
-
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ValidAnagram {
+public class ValidAnagramMain {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
 
-	String s = "aacc";
-	String t = "caac";
+        String s = "anagram";
+        String t = "nagaram";
 
-	System.out.println(isAnagram(s, t));
+        System.out.println(isAnagram_optimized2(s,t));
 
     }
 
-    // Recommended
-
-    // Time Comlexity: O(N)
+    // Time Complexity: O(NLogN)
     // Space Complexity: O(1)
-    public static boolean isAnagram1(String s, String t) {
-
-	if (s.length() != t.length())
-	    return false;
-
-	int[] count = new int[26];
-
-	for (int i = 0; i < s.length(); i++) {
-	    count[s.charAt(i) - 'a']++;
-	    count[t.charAt(i) - 'a']--;
-	}
-
-	for (int i : count) {
-	    if (i != 0)
-		return false;
-	}
-
-	return true;
-
-    }
-
-    // Time Complexity: O(S) or O(T)
-    // SPace Complexity: O(S)
     public static boolean isAnagram(String s, String t) {
 
-	if (s.length() != t.length())
-	    return false;
+        if(s.length() != t.length())
+            return false;
 
-	Map<Character, Integer> map = new HashMap<>();
-	
-	for (char c : s.toCharArray()) {
-	    map.put(c, map.getOrDefault(c, 0) + 1);
-	}
+        char[] s1 = s.toCharArray();
+        char[] t1 = t.toCharArray();
 
-	for (char c : t.toCharArray()) {
-	    if (map.containsKey(c)) {
-		map.put(c, map.get(c) - 1);
-
-	    } else {
-		return false;
-	    }
-	}
-
-	for (int i : map.values()) {
-	    if (i != 0)
-		return false;
-	}
-	return true;
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+        for(int i=0; i<s1.length; i++){
+            if(s1[i] != t1[i])
+                return false;
+        }
+        return true;
     }
 
-    // Time Complexity: O(NlogN)
-    // SPace Complexity: O(1)
-    public static boolean isAnagram2(String s, String t) {
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public static boolean isAnagram_optimized(String s, String t) {
+        if(s.length() != t.length())
+            return false;
 
-	if (s.length() != t.length())
-	    return false;
+        Map<Character,Integer> map = new HashMap<>();
 
-	char[] s1 = s.toCharArray();
-	char[] t1 = t.toCharArray();
-	java.util.Arrays.sort(s1);
-	java.util.Arrays.sort(t1);
-
-	return java.util.Arrays.equals(s1, t1);
+        for(int i=0; i<s.length(); i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)+1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0)-1);
+        }
+        for(int i : map.values()){
+            if(i!=0)
+                return false;
+        }
+        return true;
     }
 
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public static boolean isAnagram_optimized2(String s, String t) {
+        if(s.length() != t.length())
+            return false;
+
+        int[] arr = new int[26];
+
+        for(int i=0 ;i<s.length(); i++){
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+        }
+        for(int i : arr){
+            if(i!=0)
+                return false;
+        }
+        return true;
+    }
 }
